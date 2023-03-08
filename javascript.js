8dd8743b4ee31922c38c67827a6f66e98fb518d0
tito <tito657175>
2023-03-07 23:34:23 -0800

    clean up readability of code and start to isolate functions to a single task

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ let win = "Congrats, U won this round!";
 let loose = "Sorry, you lost this round.";
 let tie = "Tie, try again.";
 let invalid = "Invalid entry ya fu, try rock, paper or scissors. Game is not case sensitive, don't worry.";
-let userSelection = "nothing yet";
 let total_ties = 0;
 let total_wins = 0;
 let total_looses = 0;
@@ -13,85 +12,82 @@ function start(input){
     document.getElementById("Messages").innerHTML = input;
     console.log('input inside the function start() is ' + input);
     reachingOut(input);
-  }
+}
 
 function reachingOut(e){
     console.log("inside reachingOut function the selection is " + e);
 }
 
 // game function that returns counted results
-function startGame(userSelection){
-    if (userSelection !== "nothing yet"){
-        for (count = 1; count < 4; count++) {
+    // for (count = 1; count < 4; count++) {
+        //function that gets computer choice
+        function getComputerChoice(computerChoice) {
             let randomNumber = Math.random();
-            //function that gets computer choice
-            function getComputerChoice(computerChoice) {
-                if (randomNumber <= .33){
-                    computerChoice = "rock";
-                    return computerChoice;
-                } else if ( randomNumber >.33 && randomNumber <.63) {
-                    computerChoice = "paper";
-                    return computerChoice;
-                } else {computerChoice = "scissors";
-                    return computerChoice;
-                }
-            }
-        
-            //turns function choice into variable
-            let computerChoice = getComputerChoice();
-            // shows computer choice
-            console.log("computer picks: " + computerChoice);
-            
-            //single variable comparison string
-            let together = computerChoice + " " + userSelection;
-            
-            function gameSelection(together) {
-            if (userSelection == computerChoice){
-                total_ties++;
-                return tie;
-            } else if (together == "rock paper" 
-                || together == "paper scissors" 
-                || together == "scissors rock"){
-                total_wins++;
-                return win;
-            } else if (together == "rock scissors" 
-                || together == "paper rock" 
-                || together == "scissors paper"){
-                total_looses++;
-                return loose;
-            } else {
-                total_invalid++;
-                return invalid;
+            if (randomNumber <= .33){
+                computerChoice = "rock";
+                return computerChoice;
+            } else if ( randomNumber >.33 && randomNumber <.63) {
+                computerChoice = "paper";
+                return computerChoice;
+            } else {computerChoice = "scissors";
+                return computerChoice;
             }
         }
-    }
-    
-    }
-}
-    //turns function into string
-    // let gameResults = gameSelection();
-    // console.log("The computer picked " + computerChoice + " and you picked " + userSelection);
-    // console.log(gameResults);
+        
+        //turns function choice into variable
+        // let computerChoice = getComputerChoice();
+        // shows computer choice
+        console.log(getComputerChoice());
+        
+        //single variable comparison string
+        let together = getComputerChoice() + " " + userSelection;
+        
+        //game selector function start
+        function gameSelection(together) {
+        if (userSelection == computerChoice){
+            total_ties++;
+            return tie;
+        } else if (together == "rock paper" 
+            || together == "paper scissors" 
+            || together == "scissors rock"){
+            total_wins++;
+            return win;
+        } else if (together == "rock scissors" 
+            || together == "paper rock" 
+            || together == "scissors paper"){
+            total_looses++;
+            return loose;
+        } else {
+            total_invalid++;
+            return invalid;
+        }
+        }
+// }
+// turns function into string
+// let gameResults = gameSelection();
+// console.log("The computer picked " + computerChoice + " and you picked " + userSelection);
+// console.log(gameResults);
 
-    // display results to html
-    // let results = "The computer picked " + computerChoice + " and you picked " + userSelection + ". " + gameResults;
-    
-    // console.log("End Of Round " + count);
-    // console.log("--------------------------");
+// display results to html
+let results = "The computer picked " + computerChoice + " and you picked " + userSelection + ". " + gameResults;
+
+//console.log("End Of Round " + count);
+//console.log("--------------------------");
 
 //return results to user
 // alert(results);
 
 //Collect all results here
-// document.write(results);
-// document.write("<br> End Of Round " + count + ". <br><br>");
+/* document.write(results);
+document.write("<br> End Of Round " + count + ". <br><br>");
+*/
 
-
-//Final Count
-// document.write("Total times you tied: " + total_ties + "<br>");
-// document.write("Total times you won: " + total_wins + "<br>");
-// document.write("Total times you lost: " + total_looses + "<br>");
-// document.write("Total times you screwed up the text: " + total_invalid + "<br><br>");
+/* Final Count
+document.write("Total times you tied: " + total_ties + "<br>");
+document.write("Total times you won: " + total_wins + "<br>");
+document.write("Total times you lost: " + total_looses + "<br>");
+document.write("Total times you screwed up the text: " + total_invalid + "<br><br>");
+*/
 
 function final_results() {
 if (total_wins > total_looses) {
